e55f15d177037d8438abac6678e90812275075f4
Dan Benson <dbenson3091@gmail.com>
2021-10-19 14:31:04 -0500

    updated component in Game

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,9 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const potential =
+  "http://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2";
+const BASE_URL = "http://deckofcardsapi.com/api/deck/";
 /** Order entering system before it ships.
  *
  * Props:
@@ -8,9 +13,49 @@ import {useState, useEffect} from "react"
  * - deckOfCards, isLoading, isDeckDone
  *
  * App->Game->Card
+ *
  */
-function Game (){
-    const [deckOfCards, setDeckOfCards] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [isDeckDone, setIsDeckDone] = useState(false);
+
+async function drawNewDeck() {
+  const response = await axios({ url: `${BASE_URL}new/` });
+
+  console.log(response.data.deck_id, "response from drawNewDeck");
+  return response.data.deck_id;
 }
+
+function Game() {
+  const [deckOfCards, setDeckOfCards] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDeckDone, setIsDeckDone] = useState(false);
+
+  useEffect(function fetchDeckWhenMounted() {
+    const response = drawNewDeck();
+
+    console.log(response, "response data of useEffect");
+    setDeckOfCards(response);
+    console.log(deckOfCards, "deck of cards inside useEffect");
+    setIsLoading(false);
+  }, []);
+
+  async function drawCard() {
+    try {
+      const response = await axios({
+        url: `${BASE_URL}${deckOfCards}/draw/?count=2`,
+      });
+      console.log(response, "response worked from drawCard");
+      return response;
+    } catch (err) {
+      setIsDeckDone(true);
+    }
+  }
+
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      <button onClick={drawCard}>Draw Card</button>
+      {isDeckDone && <p>Card deck is empty</p>}
+    </div>
+  );
+}
+
+export default Game;
